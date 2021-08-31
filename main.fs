22489c7945d6a04a29e8fac6a2c74aa3d1f5e708
afry <9861676+afry23@users.noreply.github.com>
2021-08-31 14:13:26 +0000

    Simplified solution

--- main.fs
+++ main.fs
@@ -2,23 +2,45 @@ open System
 
 let tryParesInt (number:string) =
   let (success, value) = Int32.TryParse number
-  if success then Some value else None
+  if success then 
+    if value >= 0 && value <= 10 then
+      Some value 
+    else None
+  else
+    None
 
 let f n arr =
-  for item in arr do
-    for i = 1 to n do
-      printfn "%d" item
+  seq {
+    for item in arr do
+      for i = 1 to n do
+        yield item
+  }
     
 [<EntryPoint>]
 let main argv =
   let input = Console.ReadLine() |> int
+  let number = 
+    if input >= 1 && input <= 100 then
+      input
+    else 
+      0
+  
   let mutable read = true
-  while read do
-    let result = tryParesInt (Console.ReadLine())
-    read <- match result with
-            | Some _ -> true
-            | _ -> false
-  let times = 
-    if input >= 0 && input <= 10 then input 
-    else 0
+  let mutable values = []
+  let items =
+    [
+      while read do
+        let (success, value) = Console.ReadLine() |> Int32.TryParse
+        if success then
+          yield value
+        else
+          yield -1
+        read <- success
+    ]
+  let arr = List.filter (fun x -> x >= 0 && x <= 10) items 
+  printfn "%A" arr
+  
+  let output = f number arr
+  for i in output do
+    printfn "%d" i
   0
