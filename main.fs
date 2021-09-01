2b25bb1a22ee456d85d4d57feb2ff5a1e6a0c502
afry <9861676+afry23@users.noreply.github.com>
2021-09-01 10:47:11 +0000

    Evaluating e^x (WIP)

--- main.fs
+++ main.fs
@@ -1,28 +1,11 @@
 open System
 
-let f arr =
-  let foo =
-    arr
-    |> List.mapi (fun i x -> if i % 2 <> 0 then Some x else None)
-    |> List.choose id
-  foo
+let f number =
+  for i = 1 to number do
+    printfn "%d" i
   
 [<EntryPoint>]
 let main argv =
-  let list =
-    [
-      let mutable read = true
-      while read do
-        let (success, value) = Console.ReadLine() |> Int32.TryParse
-        if success then
-          yield Some value
-        else
-          yield None
-        read <- success
-    ]
-    |> List.choose id
-  
-  let output = f list
-  for i in output do
-    printfn "%d" i
+  let a = Console.ReadLine() |> int
+  f a
   0
